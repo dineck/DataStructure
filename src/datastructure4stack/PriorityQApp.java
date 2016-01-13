6c898af6097e7f42c8688dd06854ad35d98d9771
admin <admin@admin-PC>
2016-01-13 23:04:01 +0200

    1/13/2016

---
+++ src/datastructure4stack/PriorityQApp.java
@@ -0,0 +1,20 @@
+package datastructure4stack;
+
+import java.io.IOException;
+
+class PriorityQApp {
+    public static void main(String[] args) throws IOException {
+        PriorityQ thePQ=new PriorityQ(5);
+        thePQ.insert(30);
+        thePQ.insert(50);
+        thePQ.insert(10);
+        thePQ.insert(40);
+        thePQ.insert(20);
+        
+        while(!thePQ.isEmpty()) {
+            long item=thePQ.remove();
+            System.out.print(item+" ");
+        }
+        System.out.println("");
+    }
+}
